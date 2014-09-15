89be54c91915a8f703e2a403794bd6c9265ac32b
Ramsey Nasser <ram@nas.sr>
2014-09-14 23:54:06 -0400

    Make progress on squiggle

--- Assets/Clojure/Scripts/squiggle.clj
+++ Assets/Clojure/Scripts/squiggle.clj
@@ -8,25 +8,37 @@
     (if (< i (count vs))
       (do 
         (.SetPosition lr i (nth vs i))
-        (recur (inc i)))))
-  (comment (doseq [[^int i ^Vector3 v] (map vector (range) vs)]
-    (.SetPosition lr i v))))
+        (recur (inc i))))))
 
-(defcomponent Squiggle [trails ^int size]
+(defn next-point [v]
+  (Vector3/op_Addition
+    (Vector3/op_Addition v
+                         (Vector3/op_Multiply
+                           speed
+                           UnityEngine.Random/insideUnitSphere))))
+
+(defcomponent Squiggle [trails
+                        ^int size
+                        ^float speed]
   (Awake [this]
     (require 'squiggle))
 
   (Start [this]
+    (let [r (rand)
+          g (rand)
+          b (rand)]
+      (.. this (GetComponent LineRenderer) (SetColors
+                                             (Color. r g b 0)
+                                             (Color. r g b))))
     (set! trails (atom (map vec (partition size 1 (iterate
-                  #(Vector3/op_Addition % UnityEngine.Random/insideUnitSphere)
-                  Vector3/zero))))))
+                                                    next-point Vector3/zero))))))
   
   (Update [this]
     (set-line
       (.. this (GetComponent LineRenderer))
       (CatmullRomSpline/Generate
         (into-array Vector3 (first @trails))
-        5)) ; (catmul-rom (first @trails) 5)
+        5))
     (swap! trails next)))
 
 (defcomponent LookAtSquiggle [^Squiggle squiggle ^float follow]
@@ -45,44 +57,5 @@
             rot))))
 
 (defn clone-squiggle []
-  (let [^GameObject s (GameObject/Find "Squiggle")
-        news (GameObject/Instantiate s)]
-        (set! (.. news (GetComponent LineRenderer) material color)
-              (Color. (rand) (rand) (rand)))))
-
-
-(comment (defn catmul-rom* [^Vector3 p0
-                   ^Vector3 p1
-                   ^Vector3 p2
-                   ^Vector3 p3
-                   t]
-  (let [t  (float t)
-        t0 (float (* (- (* (+ (- t) 2) t) 1) t 0.5))
-        t1 (float (* (+ (* (* (- (* 3 t) 5) t) t) 2) 0.5))
-        t2 (float (* (+ (* (+ (* -3 t) 4) t) 1) t 0.5))
-        t3 (float (* (- t 1) t t 0.5))]
-  (Vector3.
-    (+ (* t0 (.x p0))
-       (* t1 (.x p1))
-       (* t2 (.x p2))
-       (* t3 (.x p3)))
-    (+ (* t0 (.y p0))
-       (* t1 (.y p1))
-       (* t2 (.y p2))
-       (* t3 (.y p3)))
-    (+ (* t0 (.z p0))
-       (* t1 (.z p1))
-       (* t2 (.z p2))
-       (* t3 (.z p3))))))
-
-(defn catmul-rom
-  ([vs] (catmul-rom vs 20))
-  ([vs samples]
-    (for [n (range 1 (- (count vs) 2))
-          i (range samples)]
-      (catmul-rom* 
-        (vs (- n 1))
-        (vs n)
-        (vs (+ n 1))
-        (vs (+ n 2))
-        (* i (/ 1.0 samples)))))))
+  (let [^GameObject s (GameObject/Find "Squiggle")]
+    (GameObject/Instantiate s)))
